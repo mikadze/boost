e71c9dbe6ee6a4b306b468a597b27e1eecdccd43
mika <mikadze.davit@gmail.com>
2026-01-03 20:37:07 +0400

    fixl worker

--- apps/worker/src/worker.controller.ts
+++ apps/worker/src/worker.controller.ts
@@ -3,11 +3,6 @@ import { EventPattern, Payload } from '@nestjs/microservices';
 import { RawEventMessage } from '@boost/common';
 import { WorkerService } from './worker.service';
 
-interface KafkaMessage<T> {
-  key: string;
-  value: T;
-}
-
 @Controller()
 export class WorkerController {
   private readonly logger = new Logger(WorkerController.name);
@@ -15,8 +10,8 @@ export class WorkerController {
   constructor(private workerService: WorkerService) {}
 
   @EventPattern('events.raw')
-  async handleEvent(@Payload() data: KafkaMessage<RawEventMessage>) {
-    this.logger.debug(`Received event: ${data.value.event} for project ${data.value.projectId}`);
-    await this.workerService.processEvent(data.value);
+  async handleEvent(@Payload() data: RawEventMessage) {
+    this.logger.debug(`Received event: ${data.event} for project ${data.projectId}`);
+    await this.workerService.processEvent(data);
   }
 }
